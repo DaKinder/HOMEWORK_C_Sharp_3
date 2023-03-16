1d21a36f4dcdeea9fd9d89820b6ce4241b9cbb5a
Alexandr <business-is-real@outlook.com>
2023-03-16 09:46:47 +0300

    refactoring

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿//Задача 19. Программа принмает на вход число и проверяет, является ли оно палиндромом.
 
-//Задача 19. Программа принмает на вход число и проверяет, является ли оно палиндромом.
-
-int FindNumberLength(int input)
+//поиск длины числа
+int FindNumberLength(int input) 
 {   
     int numLength = 0;
     
@@ -14,6 +13,7 @@ int FindNumberLength(int input)
     return numLength;
 }
 
+ //возведение 10 в степень длины числа чтобы получить divider
 int MathPowInt(int num, int exponentiation)
 {
     int i = 1;
@@ -26,9 +26,10 @@ int MathPowInt(int num, int exponentiation)
     return num;
 }
 
-//123 1*100 2*10 3*1 - 3*100 2*10 1*1 321
-
-int ReverseNumber(int divider, int num, int length) //12345 10000 50000 + 4000 + 300 +20 + 1 = 54321
+//Принцып разворота числа. Сам придумал, без гугла
+//123 = 1*100 2*10 3*1 - 3*100 2*10 1*1 = 321
+//12345 =  (divider = 100) - 300 + 20 + 1 = 321
+int ReverseNumber(int divider, int num, int length) 
 {
     int i = 0;
     int newNum = 0;
@@ -42,6 +43,7 @@ int ReverseNumber(int divider, int num, int length) //12345 10000 50000 + 4000 +
     return newNum;
 }
 
+ //проверка числа на палиндромность
 bool CheckPalindrom(int input, int reversedNum)
 {
     if(input == reversedNum)
@@ -52,23 +54,21 @@ bool CheckPalindrom(int input, int reversedNum)
 
 }
 
+ //вывод в консоль
 void PrintResult(bool result)
 {
     if(result) System.Console.WriteLine("палиндром");
     else System.Console.WriteLine(" не палиндром");
 }
 
+//блок ввода польхователем
 Console.Write("Введите число, проверим является ли оно палиндромом: ");
 int input = Convert.ToInt32(Console.ReadLine());
 
+//блоки обработки входящей информации
 int length = FindNumberLength(input);
-
 int divider = MathPowInt(10, (length -1));
-
 int reversedNum = ReverseNumber(divider, input, length);
 
+//блок вывода информации
 PrintResult(CheckPalindrom(input, reversedNum));
-
-//Задача 21. Программа принимает на вход координаты двух точек и находит расстояние между ними в 3Д пространстве
-
-//Задача 23. Программа принимает на вход число N и выдаёт таблицу кубов чисел от 1 до N/
